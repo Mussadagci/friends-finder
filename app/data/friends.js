7f4fe5f596c586cdb6878099a3e7ed11659d66f3
Your Name <you@example.com>
2019-09-12 14:29:13 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+
+var friendMatch = [
+    {
+      name: "Lubran James",
+      photo:  "https://amp.businessinsider.com/images/5a09d0563dbef426008b5a11-1136-568.jpg",
+      scores: ["5", "1", "4","4","5", "1", "2", "5", "4", 1]
+    }, {
+      name: "Stephen Curry",
+      photo: "https://www.businessinsider.com/stephen-curry-letter-girls-shoes-riley-2018-11",
+      scores: ["3", "2", "6", "4","5", "1", "5", "4", 1]
+    }, {
+      name: " Enes Kanter",
+      photo: "https://amp.businessinsider.com/images/59232d0234911b89018b5704-1334-1001.jpg",
+      scores: ["4", "1", "1", "3", "1", "5" , "2", "1",1]
+    }, {
+      name: "kevin durant",
+      photo: "https://amp.businessinsider.com/images/5b18b1841ae66219008b49f7-1334-667.jpg",
+      scores: ["3", "6", "4", "3", "1", "2", "1", "3", 1]
+    },{
+
+      name: "Klay Thompson",
+      photo: "https://amp.businessinsider.com/images/5b154c591ae6624d008b4af8-1334-667.jpg",
+      scores: ["4","3", "2", "5", "1", "2", "1", "4", 1]
+}
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
